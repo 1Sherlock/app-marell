eb1c61401b610731a7484b4149343ffdc5e0a272
1Sherlock <=>
2021-12-09 00:30:13 +0500

    project created

---
+++ app-marell-server/src/main/java/uz/soft/cosmos/appmarellserver/AppMarellServerApplication.java
@@ -0,0 +1,13 @@
+package uz.soft.cosmos.appmarellserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMarellServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMarellServerApplication.class, args);
+    }
+
+}
---
+++ app-marell-server/src/test/java/uz/soft/cosmos/appmarellserver/AppMarellServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.soft.cosmos.appmarellserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMarellServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
